c9935f5b30dc394b0a24dff463117ca8b8e83d91
trang6789 <thuytrang16071989@gmail.com>
2020-03-26 23:06:54 +0700

    first commit for Array

---
+++ src/main/java/Array/AdditiveInverse.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class AdditiveInverse {
+    public static int[] additiveInverse(int[] arr) {
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = 0-arr[i];
+        }
+        return arr;
+    }
+    public static  void printArray(int[] arr){
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(" "+arr[i]+" ");
+        }
+    }
+    public static void main(String[] args){
+        int[] array= {1,2,-3,8};
+        int[] array_new = additiveInverse(array);
+        printArray(array_new);
+
+        }
+}
