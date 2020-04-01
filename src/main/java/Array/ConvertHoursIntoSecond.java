a461b060f09b66d0c3f26dc0239bdf20ff0089fd
trang6789 <thuytrang16071989@gmail.com>
2020-04-02 00:43:59 +0700

    Fifth commit for Array

---
+++ src/main/java/Array/ConvertHoursIntoSecond.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class ConvertHoursIntoSecond {
+    public static int howManySecond(int hours){
+        int second = hours*3600;
+        return second;
+    }
+    public static void main(String[] args){
+        int hours = 2;
+        System.out.println(howManySecond(hours));
+    }
+}
---
+++ src/main/java/Array/FarmProblem.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class FarmProblem {
+    public static int animals(int chickens, int cows, int pigs){
+        int total = chickens * 2 + (cows+ pigs)*4;
+        return  total;
+    }
+
+    public static void main(String[] args){
+        int chickens =2;
+        int cows = 3;
+        int pigs = 5;
+        System.out.println(animals(chickens,cows,pigs));
+    }
+}
