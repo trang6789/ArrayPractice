b85cd330507fba92cabeabd49279890c44ebe2c8
trang6789 <thuytrang16071989@gmail.com>
2020-04-06 18:43:17 +0700

    tenth commit for Array

---
+++ src/main/java/Array/FreeCoffeeCups.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class FreeCoffeeCups {
+    public static int totalCups(int n) {
+
+        return n = n+(n/6);
+
+    }
+    public static void main(String[] args){
+        int n = 8;
+        System.out.println(totalCups(n));
+    }
+}
